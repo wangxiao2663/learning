49dcc1b965490543b3af4da0afb982324afa7dd6
wangxiao <wangxiao3@myhexin.com>
2016-01-16 16:27:52 +0800

    2-commit

---
+++ my_learning_python/IO/print_input.py
@@ -0,0 +1,22 @@
+print 'i','am','wangxiao' # , auto print a blank
+print '100+200=',100+200  # 'string', calculate
+
+print 'what\'s your name'
+name = raw_input()	#try to enter a name
+print 'hello',name #print your name
+
+print 'let\'s learning \nPython\n'
+
+print '\\\\\\'   # \\ = \ transferred meaning
+print r'\\\\\\'  # r'string'   string in r'' not transferred meaning
+print '|\t\t|'	
+print r'|\t\t|'
+
+print '''    
+woca
+nima
+asd
+'''                  # int ''' str '''  we can use more line to print
+
+
+
---
+++ my_learning_python/operator/bool.py
@@ -0,0 +1,17 @@
+print True and True
+print True and False
+print False and False
+
+print True or True
+print True or False
+print False or False
+
+print not True
+print not False
+
+age = raw_input()
+age = 12
+if age >= 18:
+    print 'adult'
+else:
+    print 'child'
