aaab51b559138dfd3328df801495575a67e007cb
wangxiao <wangxiao3@myhexin.com>
2016-01-16 17:37:45 +0800

    rename operator to variate

---
+++ my_learning_python/variate/variate.py
@@ -0,0 +1,26 @@
+a = 123			# a is a number (int ? )
+print a
+answer = True		# answer is a bool
+print answer
+a = 'str'		# a is a str
+print a
+
+#---------------------------------------
+
+x = 10
+x = x + 2
+print x
+	
+x = 10 / 3 		 
+print x
+
+x = 10.0 / 3
+print x
+
+#----------------------------------------
+PI = 3.1415926		#use Caps  as constant value, but you can also change it 
+print PI
+
+PI = 2
+print PI
+
