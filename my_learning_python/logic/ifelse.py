73a343a6fbe836637daedac3e1f5231c991ed93d
wangxiao <wangxiao3@myhexin.com>
2016-01-26 19:22:08 +0800

    4

---
+++ my_learning_python/logic/ifelse.py
@@ -0,0 +1,22 @@
+age = 20
+
+age = input('enter your age:') 
+
+if age > 18:
+	print('adult')
+else:
+	print('child')
+
+age = input('enter your age again:')
+
+if age > 50:
+	print('old man')
+elif age > 18:
+	print('adult')
+else:
+	print('child')
+
+b = True
+
+if b:
+	print('yeah')
