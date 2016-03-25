5f2e50ecc1a834ebf0d89e0145eab7c1504f59c6
wangxiao <wangxiao2663@gmail.com>
2016-03-25 11:41:50 +0800

    123

--- scripts/delay_dir/server.php
+++ scripts/delay_dir/server.php
@@ -4,8 +4,8 @@ define("_DOC_ROOT", dirname(__FILE__));
 require_once(_DOC_ROOT."/config.php");
 require_once(_DOC_ROOT."/common.php");
 
-define('_SERVER_IP', '192.168.200.132');
-define('_SERVER_PORT', 9501);
+define('_SERVER_IP', '172.31.60.33');
+define('_SERVER_PORT', 9888);
 
 define('_START_HEAD', 'fdfdfdfd');
 define('_END_TAIL', 'DONEDONEDONE');
@@ -17,7 +17,7 @@ connect($redis);
 
 $serv = new swoole_server(_SERVER_IP, _SERVER_PORT); 
 $serv->on('connect', function ($serv, $fd) {  
-    echo "Client: Connect.\n";
+    echo "Connect : ".$fd."\n";
 });
 
 $serv->on('receive', function ($serv, $fd, $from_id, $data) {
@@ -27,13 +27,14 @@ $serv->on('receive', function ($serv, $fd, $from_id, $data) {
     {
         if(strcmp($data, _START_HEAD) == 0)
         {
-            echo "hello.\n";
+        	echo "start receive from ".$fd."\n";
             $recv_msg_arr[$fd] = "";
         }
         if(strcmp($data, _END_TAIL) == 0)
         {
-            echo "done!\n";
+        	echo "end receive from ".$fd."\n";
             Analysis($recv_msg_arr[$fd]);
+            $recv_msg_arr[$fd] = "";
         }
     }
     else
@@ -47,10 +48,10 @@ $serv->on('receive', function ($serv, $fd, $from_id, $data) {
 });
 
 $serv->on('close', function ($serv, $fd) {
-    echo "Client: Close.\n";
+    echo "Client ".$fd." : Close.\n";
 });
 
-$serv->start(); 
+$serv->start();
 
 
 
@@ -58,32 +59,48 @@ $serv->start();
 function Analysis($data)
 {
 	global $redis;
+	$from_ip = "";
+	$message = "";
+
 	try 
 	{
 		$from_ip = strchr($data,":",true);
     	$message = substr($data, strlen($from_ip) + 1);
-        stripslashes($message);
-    	$Now = date("YmdH");
-
-        $redis->set($from_ip, $message);
-
-        $hisKey = $from_ip."_".$Now;
-        $redis->set($hisKey, $message);
-        $redis->expire($hisKey, 3600 * 48);
-
-    	$msg = $redis->get($from_ip);
     }
     catch (Exception $e)
     {
-   		echo "error!";
+    	echo "mem error!\n";
+		return;
+    }
+	if (empty($from_ip) || $from_ip == "" || strlen($from_ip) == 0)
+	{
+		echo "empty !!!\n";
+		return;
+	}
+	
+	
+    try
+    {
+    	$Now = date("YmdH");
+        $redis->set($from_ip, $message);
+        $hisKey = $from_ip."_".$Now;
+        $redis->set($hisKey, $message);
+		echo "set ".$hisKey."\n";
+        $redis->expire($hisKey, 3600 * 48);
+        echo "time : ".$Now.", ip : ".$from_ip."\n";
+    }
+    catch (Exception $e)
+    {
+        foreach ($recv_msg_arr as $key => $value) 
+        {
+            $recv_msg_arr[$key] = "";
+        }
+        echo "error!";
+		$redis->close();
+        connect($redis);
     }
 }
 
 
-
-function Send($data_array)
-{
-
-}
 
 ?>
